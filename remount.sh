cb708daa5bd8a168153c3c116a16962bd8f66d91
tsvtitan <tsv.titan@gmail.com>
2016-02-18 10:20:29 +0300

    No messages

--- remount.sh
+++ remount.sh
@@ -8,11 +8,11 @@ HOME=`dirname $0`
 sudo umount -f $HOME/node
 sudo umount -f $HOME/www
 
-echo "Mounting node source ..."
+echo "Mounting node source..."
 
 echo "root" | $(which sshfs) root@radius.ml:/root/.nvm/versions/node/v4.2.4 $HOME/node -p 50022 -o allow_other -o cache=no -o reconnect -o password_stdin
 
-echo "Mounting www ..."
+echo "Mounting www..."
 
 echo "root" | $(which sshfs) root@radius.ml:/www $HOME/www -p 50022 -o allow_other -o cache=no -o reconnect -o password_stdin
 
